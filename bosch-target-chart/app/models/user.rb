c8979cf2fc951f90da5f3553fc36e6cf31d9616e
Kyle Glick <glickk@musc.edu>
2018-02-17 13:39:51 -0500

    add devise gem with config + user model

---
+++ bosch-target-chart/app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  ##### Include default devise modules. (See Docs: https://github.com/plataformatec/devise)
+  ##### Options:
+  # :database_authenticatable - hashes and stores a password in the database to validate the authenticity of a user while signing in. The authentication can be done both through POST requests or HTTP Basic Authentication.
+  # :omniauthable             - adds OmniAuth (https://github.com/omniauth/omniauth) support.
+  # :confirmable              - sends emails with confirmation instructions and verifies whether an account is already confirmed during sign in.
+  # :recoverable              - resets the user password and sends reset instructions.
+  # :registerable             - handles signing up users through a registration process, also allowing them to edit and destroy their account.
+  # :rememberable             - manages generating and clearing a token for remembering the user from a saved cookie.
+  # :trackable                - tracks sign in count, timestamps and IP address.
+  # :timeoutable              - expires sessions that have not been active in a specified period of time.
+  # :validatable              - provides validations of email and password. It's optional and can be customized, so you're able to define your own validations.
+  # :lockable                 - locks an account after a specified number of failed sign-in attempts. Can unlock via email or after a specified time period.
+  devise :database_authenticatable, :confirmable, :recoverable,
+          :registerable, :trackable, :timeoutable, :lockable
+
+  validates_presence_of :first_name, :last_name
+  
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: Devise::email_regexp }
+
+  validates :password,
+            presence: true,
+            confirmation: true,
+            length: { within: Devise::password_length }
+end
