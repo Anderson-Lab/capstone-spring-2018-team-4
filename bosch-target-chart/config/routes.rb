e9d3daeb5a2ef535e82ff255f0f4e5183ec118b9
Joshua Glass <glassja@g.cofc.edu>
2018-02-26 20:51:54 -0500

    Added user dashboard, no views yet

--- bosch-target-chart/config/routes.rb
+++ bosch-target-chart/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { registrations: 'registrations' }
 
+  resources :dashboard, only: [:index]
+
   root to: 'home#index'
 end
