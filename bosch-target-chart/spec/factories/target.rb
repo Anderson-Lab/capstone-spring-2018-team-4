b9f464ac64e9e4172d83b726f8080da8ead32680
Kyle Glick <kyleglick99@gmail.com>
2018-03-09 20:58:24 -0500

    fixed specs

--- bosch-target-chart/spec/factories/target.rb
+++ bosch-target-chart/spec/factories/target.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     unit_type         { Target::UNIT_TYPES[1] }
     update_frequency  { [I18n.t(:targets)[:fields][:update_frequency][:monthly], I18n.t(:targets)[:fields][:update_frequency][:yearly]].sample }
     comments          { Faker::Lorem.sentence }
-    year              { 1970 + Random.rand(100) }
+    year              { Time.now.year }
 
     trait :numerical do
       unit_type { Target::UNIT_TYPES[0] }
