c3edbfe0b0188c3587299c5ca8c6df48386c1503
Samantha Word <samword245@gmail.com>
2018-03-10 09:33:23 -0500

    Removed old method

--- bosch-target-chart/app/helpers/targets_helper.rb
+++ bosch-target-chart/app/helpers/targets_helper.rb
@@ -1,11 +1,4 @@
 module TargetsHelper
-  def edit_target_button(target)
-    link_to 'javascript:void(0)', class: 'btn btn-warning text-white edit-target-button',
-    title: t(:actions)[:edit], data: { toggle: 'tooltip', animation: 'false' } do
-      fa_icon 'edit'
-    end
-  end
-
   def delete_target_button(target)
     link_to 'javascript:void(0)', class: 'btn btn-danger text-white delete-target-button',
       data: { target_id: target.id } do
