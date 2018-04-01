c060c64cd974ce1e1ea3deabc490a9ed914fc5e8
Samantha Word <samword245@gmail.com>
2018-04-01 16:51:21 -0400

    Fixed bug, ymlized text

--- bosch-target-chart/app/helpers/categories_helper.rb
+++ bosch-target-chart/app/helpers/categories_helper.rb
@@ -1,7 +1,7 @@
 module CategoriesHelper
   def color_select_options
     color_options = {}
-    t(:categories)[:chart_colors].each{ |color_key, color_name| color_options.merge!(color_name => Category::COLOR_HEX_VALUES[color_key]) }
+    t(:categories)[:chart_colors].each{ |color_key, color_name| color_options.merge!(color_name => Category::COLOR_HEX_VALUES_HASH[color_key]) }
     color_options
   end
 end
--- bosch-target-chart/app/models/category.rb
+++ bosch-target-chart/app/models/category.rb
@@ -2,10 +2,10 @@ class Category < ApplicationRecord
   has_many :targets
   has_attached_file :icon, default_url: ActionController::Base.helpers.asset_path("categories/default_category_icon.png")
 
-  COLOR_HEX_VALUES = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/colors.yml"))[:chart_colors]
+  COLOR_HEX_VALUES_HASH = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/colors.yml"))[:chart_colors]
 
   validates_presence_of :name, :color
-  validates_inclusion_of :color, in: COLOR_HEX_VALUES
+  validates_inclusion_of :color, in: COLOR_HEX_VALUES_HASH.values
   validates_attachment :icon, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
 
   # TODO: Before delete, unassign category from targets?
