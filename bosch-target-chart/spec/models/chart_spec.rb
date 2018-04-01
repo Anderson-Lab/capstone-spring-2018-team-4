ff208199ceee3248e2147a2a11971238445dfc0f
Kyle Glick <kyleglick99@gmail.com>
2018-04-01 12:27:37 -0400

    new year specs

--- bosch-target-chart/spec/models/chart_spec.rb
+++ bosch-target-chart/spec/models/chart_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Chart, type: :model do
+  it { is_expected.to belong_to(:department) }
+  
   it { is_expected.to have_and_belong_to_many(:targets) }
 
   it { is_expected.to validate_presence_of(:name) }
