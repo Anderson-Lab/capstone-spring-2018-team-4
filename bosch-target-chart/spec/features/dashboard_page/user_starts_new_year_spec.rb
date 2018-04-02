3c4a78694d079dc6594c2c0842e99c81f4b12a9b
Kyle Glick <kyleglick99@gmail.com>
2018-04-02 17:03:08 -0400

    add/fix specs for new year bug

--- bosch-target-chart/spec/features/dashboard_page/user_starts_new_year_spec.rb
+++ bosch-target-chart/spec/features/dashboard_page/user_starts_new_year_spec.rb
@@ -7,7 +7,26 @@ RSpec.describe "User starts a new year", js: true do
   before :each do
     current_user.confirm
     sign_in(current_user)
+  end
 
+  context 'with no existing charts present' do
+    context 'and user starts the current year' do
+      it 'should redirect to the Dashboard for the current year' do
+        @year       = Time.now.year
+        @department = FactoryBot.create(:department, name: 'Departed')
+
+        visit dashboard_path
+
+        click_link I18n.t('charts.new_year.start_button', year: @year)
+
+        expect(page).to have_current_path(dashboard_path(year: @year))
+        expect(page).to have_content(I18n.t('charts.plant_chart.header', chart_name: I18n.t(:charts)[:plant_chart][:default_name], year: @year))
+        expect(page).to have_content(I18n.t('charts.department_chart.header', department: @department.name, chart_name: I18n.t(:charts)[:department_chart][:default_name], year: @year))
+      end
+    end
+  end
+
+  it 'should redirect to the Dashboard for the new year' do
     @year       = Time.now.year
     @department = FactoryBot.create(:department, name: 'Departed')
                   FactoryBot.create(:chart, name: 'Uncharted', year: @year)
@@ -20,9 +39,7 @@ RSpec.describe "User starts a new year", js: true do
     wait_for_ajax
 
     click_link I18n.t('charts.new_year.start_button', year: @year + 1)
-  end
 
-  it 'should redirect to the Dashboard for the new year' do
     expect(page).to have_current_path(dashboard_path(year: @year + 1))
     expect(page).to have_content(I18n.t('charts.plant_chart.header', chart_name: 'Uncharted', year: @year + 1))
     expect(page).to have_content(I18n.t('charts.department_chart.header', department: @department.name, chart_name: 'Chartmander', year: @year + 1))
