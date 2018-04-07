ae5ac1fc537ac10277f74d640eb3b468516cf60d
Samantha Word <samword245@gmail.com>
2018-04-07 14:43:24 -0400

    model specs

--- bosch-target-chart/spec/models/category_spec.rb
+++ bosch-target-chart/spec/models/category_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Category, type: :model do
 
   it { is_expected.to validate_inclusion_of(:color).in_array(Category::COLOR_HEX_VALUES_HASH.values) }
 
+  it { is_expected.to validate_presence_of(:icon) }
   it { should validate_attachment_content_type(:icon).
                 allowing('image/jpeg', 'image/jpg', 'image/png', 'image/gif').
                 rejecting('text/plain', 'text/xml') }
