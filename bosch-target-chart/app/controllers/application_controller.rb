9a854dbb45a6a33e4e728c9a8b1996816c5b237d
Kyle Glick <kyleglick99@gmail.com>
2018-04-15 17:53:10 -0400

    redirect to dashboard on login from homepage

--- bosch-target-chart/app/controllers/application_controller.rb
+++ bosch-target-chart/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || super 
+    stored_location_for(resource) || dashboard_path
   end
 
   def get_year
@@ -17,11 +17,10 @@ class ApplicationController < ActionController::Base
   private 
 
   def storable_location?
-    request.get? && is_navigational_format? && !devise_controller?
+    request.get? && is_navigational_format? && !devise_controller? && controller_name != 'home'
   end
 
   def store_user_location!
     store_location_for(:user, request.fullpath)
   end
-
 end
